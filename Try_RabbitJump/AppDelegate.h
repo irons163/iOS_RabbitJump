a09f611952a7e08fff031a06bbbc9c8a4f831dc5
irons163 <irons@ironsdeMacBook-Air.local>
2015-08-06 08:30:43 +0800

    Initial Commit

---
+++ Try_RabbitJump/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Try_RabbitJump
+//
+//  Created by irons on 2015/8/6.
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
