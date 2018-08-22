d23c3d6376571c5236ddac37f0043f80baa0fb5a
irons163 <irons163@gmail.com>
2018-08-22 16:41:41 +0800

    second

--- Try_RabbitJump/AppDelegate.h
+++ Try_RabbitJump/AppDelegate.h
@@ -1,9 +1,6 @@
 //
 //  AppDelegate.h
-//  Try_RabbitJump
-//
-//  Created by irons on 2015/8/6.
-//  Copyright (c) 2015年 irons. All rights reserved.
+//  UberJump
 //
 
 #import <UIKit/UIKit.h>
@@ -12,6 +9,4 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
